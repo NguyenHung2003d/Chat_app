8f61e3ea9913be0a36852c5897dd23766246613e
lvt302000 <hn989422@gmail.com>
2025-05-19 12:25:12 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
+  
 app.use(
   cors({
     origin: "http://localhost:5173",
