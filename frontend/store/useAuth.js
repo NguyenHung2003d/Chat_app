224d66845a01facfbad680b46bce7507f175ad36
lvt302000 <hn989422@gmail.com>
2025-05-19 13:41:06 +0700

    done

--- frontend/store/useAuth.js
+++ frontend/store/useAuth.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import { toast } from "react-toastify";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE = 'development' ? "http://localhost:5000" : '/';
+const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:5000" : '/';
 
 export const useAuth = create((set, get) => ({
   authUser: null,
