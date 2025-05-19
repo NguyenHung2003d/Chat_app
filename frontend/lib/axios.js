4726b29de1de8e35478550a16c5a4039e7a044dd
lvt302000 <hn989422@gmail.com>
2025-05-19 10:08:19 +0700

    done

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -2,8 +2,5 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : '/api',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-      },
+    withCredentials: true
 })
